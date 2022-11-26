aeebec0922da982f9ccb67fa3ea156f78b51e43c
Correa7 <correadamian2019@gmail.com>
2022-11-25 22:07:02 -0300

    Version con Funciones

--- javascript.js
+++ javascript.js
@@ -1,50 +1,104 @@
 /* Ingresar un año por teclado. Mostrar si el año es Bisiesto o no; si es bisiseto, mostrar los proximos años bisiestos (4), */
 
 
+/*    Version sin funciones    */
+
+
+// let year = parseInt (prompt ("Ingresa una año, para averiguar si es Bisiesto"))
+
+// let listyears = ""
+
+
+// while (typeof year == "number") {
+
+//     if  (year <= 0 ) {
+            
+//         year = parseInt ( prompt ("Ingresa un año mayor al año 0"))
+        
+//     }
+
+//     else if (isNaN (year)) {
+
+//         year = parseInt ( prompt ("Ingresa un año en numeros (ejemplo: 1920) :"))
+
+//     }
+
+//     else if ( ( year % 4 == 0 ) && ( (year % 100 != 0 ) || ( year % 400 == 0) ) )  {
+        
+         
+//          for (let i = (year + 4) ; i <= (year + 16); i+=4) {
+             
+//              listyears = listyears + i + ", " 
+             
+//             }
+            
+            
+//         alert ("El año: " + year + ", es bisiesto, los proximos 4 años bisiestos son: " + listyears )
+
+//         break
+            
+//         }
+        
+//     else {
+//             alert("El año: " + year + ", No es bisiesto")
+        
+//             break
+//         }
+        
+// }
+    
+
+/*        Version Con funciones      */
+
+
 
 let year = parseInt (prompt ("Ingresa una año, para averiguar si es Bisiesto"))
 
 let listyears = ""
 
+function bisiesto (a) {
+
+    if ( ( a % 4 == 0 ) && ( (a % 100 != 0 ) || ( a % 400 == 0) ) ){
+
+        for (let i = (year + 4) ; i <= (year + 16); i+=4) {
+                 
+            listyears = listyears + i + ", " 
+           
+          }
+          
+          
+        alert ("El año: " + year + ", es bisiesto, los proximos 4 años bisiestos son: " + listyears )
+
+          return
+    }
+    else {
+
+        alert("El año: " + year + ", No es bisiesto")
+        return
+    }
+
+}
 
 while (typeof year == "number") {
 
-    if  (year <= 0 ) {
+        if  (year <= 0 ) {
+                
+            year = parseInt ( prompt ("Ingresa un año mayor al año 0"))
             
-        year = parseInt ( prompt ("Ingresa un año mayor al año 0"))
-        
-    }
-
-    else if (isNaN (year)) {
-
-        year = parseInt ( prompt ("Ingresa un año en numeros (ejemplo: 1920) :"))
-
-    }
-
-    else if ( ( year % 4 == 0 ) && ( (year % 100 != 0 ) || ( year % 400 == 0) ) )  {
-        
-         
-         for (let i = (year + 4) ; i <= (year + 16); i+=4) {
-             
-             listyears = listyears + i + ", " 
-             
+        }
+    
+        else if (isNaN (year)) {
+    
+            year = parseInt ( prompt ("Ingresa un año en numeros (ejemplo: 1920)"))
+    
+        }
+            
+        else {
+                bisiesto (year) 
+                break
             }
             
-            
-        alert ("El año: " + year + ", es bisiesto, los proximos 4 años bisiestos son: " + listyears )
-
-        break
-            
-        }
-        
-    else {
-            alert("El año: " + year + ", No es bisiesto")
-        
-            break
-        }
-        
-}
-    
+    }
 
 
 
